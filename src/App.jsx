f1f39dc59ed117e6aced7910abd012b9c87ee617
Ornella Altunyan <44654695+ornelladotcom@users.noreply.github.com>
2022-01-26 11:38:45 -0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// Import the DogList component
+import DogList from './DogList'
+
+function App() {
+    return (
+        <article>
+            <h1>Dog names</h1>
+            {/* Display the DogList component */}
+            <DogList />
+
+            {/* TODO: Add message */}
+            
+
+        </article>
+    )
+}
+
+export default App;
